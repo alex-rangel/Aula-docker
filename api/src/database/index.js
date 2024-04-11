f44a5f41f307b4e1dd73bcf4ec78d8afae179b49
alex-rangel <alexrangel90@gmail.com>
2024-04-11 20:59:35 -0300

    finalizei as configuraçoes pra poder rodar os containers

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoConnection = mongoose.connect('mongodb://mongo-register:27017/Usuarios')
+const mongoConnection = mongoose.connect('mongodb://mongo-register:27017/Pessoas')
     .then(() => console.log("banco de dados conectado"))
 
 module.exports = mongoConnection
